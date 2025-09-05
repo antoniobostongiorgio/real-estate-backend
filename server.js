6de6f9031a194d80cb10c0e80348caa7ea1a6f15
antoniobostongiorgio <antoniobstgiorgio@gmail.com>
2025-09-05 16:31:38 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.js";
 import annunciRoutes from "./routes/annunci.js";
 
